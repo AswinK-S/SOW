f04f6f0587a718b4f48abdbd88c0816b1c9e74ee
Aswin K S <aswinkalathur11@gmail.com>
2025-11-27 03:08:24 +0530

    created the backend folder

---
+++ Backend/db.js
@@ -0,0 +1,12 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+});
+
+export default pool;
